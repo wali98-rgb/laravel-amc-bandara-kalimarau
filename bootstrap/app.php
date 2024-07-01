c84b9f5027602c36d08b507d293df647bf9a588b
wali98-rgb <wyyuddin@gmail.com>
2024-07-01 13:59:33 +0700

    add: authentication and middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'auth.login' => \App\Http\Middleware\Authenticate::class,
+        ]);
+
+        $middleware->web(append: [
+            \RealRashid\SweetAlert\ToSweetAlert::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
