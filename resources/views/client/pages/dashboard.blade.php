350fa815653bb9748748bfacd4f0b33c35ad3e25
wali98-rgb <wyyuddin@gmail.com>
2024-07-01 17:56:29 +0700

    add: dashboard page

--- resources/views/client/pages/dashboard.blade.php
+++ resources/views/client/pages/dashboard.blade.php
@@ -13,24 +13,24 @@
                 <img src="{{ asset('frontend/img/caro1.jpg') }}" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block"
                     style="background-color: rgba(0, 30, 70, 0.3); border-radius: .3rem">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
+                    <h5>Pelaporan Kejadian AMC Bandara Kalimarau</h5>
+                    <p>Melaporkan segala bentuk kejadian di bandara Kalimarau.</p>
                 </div>
             </div>
             <div class="carousel-item" data-bs-interval="2000" style="height: 100vh">
                 <img src="{{ asset('frontend/img/caro2.jpg') }}" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block"
                     style="background-color: rgba(0, 30, 70, 0.3); border-radius: .3rem">
-                    <h5>Second slide label</h5>
-                    <p>Some representative placeholder content for the second slide.</p>
+                    <h5>AMC Bandara Kalimaru</h5>
+                    <p>Kantor Unit Penyelenggara Bandar Udara Kelas I Kalimarau.</p>
                 </div>
             </div>
             <div class="carousel-item" style="height: 100vh">
                 <img src="{{ asset('frontend/img/caro3.jpg') }}" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block"
                     style="background-color: rgba(0, 30, 70, 0.3); border-radius: .3rem">
-                    <h5>Third slide label</h5>
-                    <p>Some representative placeholder content for the third slide.</p>
+                    <h5>AMC Bandara Kalimarau</h5>
+                    <p>Unit Penyelenggara Bandar Udara di lingkungan Kementerian Perhubungan.</p>
                 </div>
             </div>
         </div>
@@ -42,5 +42,57 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
+    </div>
+@endsection
+
+@section('main_content')
+    <div class="c0nt container py-4">
+        <div class="c0nTt7L text-center mt-3 mb-5">
+            <h1>Dashboard</h1>
+        </div>
+
+        <div class="card p-3 mb-2">
+            <div class="row">
+                <div class="col-4">
+                    <img src="{{ asset('frontend/img/schedule.jpg') }}" alt="Jadwal Penerbangan" width="100%"
+                        height="100%" class="rounded">
+                </div>
+
+                <div class="col-8">
+                    <h2>Jadwal Penerbangan Bandara Kalimarau</h2>
+                    <p align="justify">
+                        Pemberitahuan Rencana Flight Aktif Super Air Jet di Bandar Udara Kalimarau Berau, Rencana
+                        penerbangan Maskapai Super Air Jet akan menggunakan Pesawat Airbus A320 dengan Seat 180 Ekonomi,
+                        Periode Efektif di jadwalkan pada Tanggal <b>14 Juni 2024</b> yang melayani penerbangan regular rute
+                        Surabaya - Berau PP <b>Setiap Hari</b> (Frekuensi 7X) dalam seminggu dengan jadwal penerbangan di
+                        sesuaikan dengan waktu setempat:
+
+                        <br><br>
+
+                        <b>IU 672 SUB - BEJ</b> <br>
+                        - SUB (15:10 wib) to BEJ (18:10 wita) <br>
+                        <b>IU 673 BEJ - SUB</b> <br>
+                        - BEJ (18:50 wita) to SUB (19:45 wib)
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card p-3">
+            <div class="row">
+                <div class="col-4">
+                    <img src="{{ asset('frontend/img/bhayangkara.jpg') }}" alt="Jadwal Penerbangan" width="100%"
+                        height="100%" class="rounded">
+                </div>
+
+                <div class="col-8">
+                    <h2>BLU UPBU Kelas I Kalimarau</h2>
+                    <p align="justify">
+                        BLU UPBU Kelas I Kalimarau mengucapkan Selamat Hari Bhayangkara Ke 78, Maju terus kepolisian
+                        Republik Indonesia.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
